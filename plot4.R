7a90a6a69b541c1a22b1031335a05a1a2462c9d9
Shadi Copty <shadi.copty@yahoo.com>
2016-04-07 22:49:52 -0700

    Submit Plot4

--- plot4.R
+++ plot4.R
@@ -1,4 +1,14 @@
 library(ggplot2)
+library(dplyr2)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-ggplot(mtcars, aes(mpg, wt)) + geom_point()
-ggsave("plot1.png")
+combustions <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coals <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustions & coals)
+combustionSCC <- SCC[coalCombustion,]$SCC
+aggr <- NEI[NEI$SCC %in% combustionSCC,]
+aggr2 <- aggregate(Emissions ~ year + type, aggr, sum)
+
+qplot(year, Emissions, data=aggr2, facets = .~type,main = "Emission from coal")
+ggsave("plot4.png")
