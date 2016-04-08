3e4565daffeff972664b01dc4a18880bcfd35d3f
Shadi Copty <shadi.copty@yahoo.com>
2016-04-07 23:04:17 -0700

    Submit 6

--- plot5.R
+++ plot5.R
@@ -6,5 +6,5 @@ SCC <- readRDS("Source_Classification_Code.rds")
 BaltimoreVehcle <- NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"),]
 aggr <- aggregate(Emissions ~ year, data=BaltimoreVehcle, sum)
 
-qplot(year, Emissions, data=aggr, main = "Baltimore emission over year by type")
+qplot(year, Emissions, data=aggr, main = "Baltimore vehicle emission over year by type")
 ggsave("plot5.png")
--- plot6.R
+++ plot6.R
@@ -2,10 +2,16 @@ library(ggplot2)
 library(dplyr2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-Baltimore <- filter(NEI, fips == "24510")
 
-Baltimore <- transform(Baltimore, year <- as.factor(year))
-aggr <- aggregate(Emissions ~ year + type, Baltimore, sum)
+BaltimoreVehcle <- NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"),]
+aggr <- aggregate(Emissions ~ year, data=BaltimoreVehcle, sum)
+aggr$City = "Baltimore"
 
-qplot(year, Emissions, data=aggr, facets = .~type,main = "Baltimore emission over year by type")
-ggsave("plot3.png")
+LaVehcle <- NEI[(NEI$fips=="06037") & (NEI$type=="ON-ROAD"),]
+aggr2 <- aggregate(Emissions ~ year, data=LaVehcle, sum)
+aggr2$City = "LA"
+
+aggr3 <- rbind (aggr, aggr2)
+
+qplot(year, Emissions, data=aggr3, color = City, main = "Vehicle emission over year by type")
+ggsave("plot6.png")
