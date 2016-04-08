ed43085a30fb40cf61f44ab53445ccebeb717b05
Shadi Copty <shadi.copty@yahoo.com>
2016-04-07 22:57:40 -0700

    submit 5

--- plot5.R
+++ plot5.R
@@ -2,10 +2,9 @@ library(ggplot2)
 library(dplyr2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-Baltimore <- filter(NEI, fips == "24510")
 
-Baltimore <- transform(Baltimore, year <- as.factor(year))
-aggr <- aggregate(Emissions ~ year + type, Baltimore, sum)
+BaltimoreVehcle <- NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"),]
+aggr <- aggregate(Emissions ~ year, data=BaltimoreVehcle, sum)
 
-qplot(year, Emissions, data=aggr, facets = .~type,main = "Baltimore emission over year by type")
-ggsave("plot3.png")
+qplot(year, Emissions, data=aggr, main = "Baltimore emission over year by type")
+ggsave("plot5.png")
