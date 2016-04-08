2b0c01e664d67ea18457abfc6da30058b19c928d
Shadi Copty <shadi.copty@yahoo.com>
2016-04-07 22:16:16 -0700

    Submitting plot 3

--- plot3.R
+++ plot3.R
@@ -1,4 +1,11 @@
 library(ggplot2)
+library(dplyr2)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+Baltimore <- filter(NEI, fips == "24510")
 
-ggplot(mtcars, aes(mpg, wt)) + geom_point()
-ggsave("plot1.png")
+Baltimore <- transform(Baltimore, year <- as.factor(year))
+aggr <- aggregate(Emissions ~ year + type, Baltimore, sum)
+
+qplot(year, Emissions, data=aggr, facets = .~type,main = "Baltimore emission over year by type")
+ggsave("plot3.png")
